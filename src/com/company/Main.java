24c8a5999a51151f0cb519141ac5a80719910551
lKaktusl <44606965+lKaktusl@users.noreply.github.com>
2018-11-07 16:25:21 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Information about student:\nName:\t Povkh Nikita\nGroup:\t AT-161\nCountry: Ukraine\nE-mail:\t server1231@yandex.ua\n");
+
+        System.out.print("Information about the student:\n" +
+                "Name:\t Povkh Nikita\n" +
+                "Group:\t AT-161\n" +
+                "Country: Ukraine\n" +
+                "E-mail:\t server1231@yandex.ua\n");
+
+    }
+}
